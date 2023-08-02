d93f5f58a8caf4e074b2a4fa648d1cc621a1d81c
Kayllanne-Evellyn <kayllanne373evellyn@gmail.com>
2023-08-02 18:41:17 -0300

    first commit

---
+++ cadastro_test.js
@@ -0,0 +1,73 @@
+Feature('cadastro');
+
+Scenario('realizar cadastro', ({ I }) => {
+
+    I.amOnPage('https://www.americanas.com.br');
+    I.click('olá, faça seu login ou cadastre-se')
+    I.click('cadastrar')
+    I.click('Nome completo')
+    I.fillField('Nome completo', 'MARIA LUIZA DA SILVA')
+    I.click('Feminino')
+    I.click('//*[@type="tel"]')
+    I.fillField('//*[@type="tel"]', ('22032000'))
+    I.click('//*[@name="cpf"]')
+    I.fillField('//*[@name="cpf"]', ('99853739011'))
+    I.click('//*[@name="phone"]')
+    I.fillField('//*[@name="phone"]', ('11988998899'))
+    I.click('//*[@name="email"]')
+    I.fillField('//*[@name="email"]', 'testeluiza@gmail.com')
+    I.click('//*[@name="password"]')
+    I.fillField('//*[@name="password"]', ('105567432'))
+    I.click('//*[@type="submit"]')
+
+});
+
+Scenario('realizar cadastro com cpf invalido', ({ I }) => {
+
+    I.amOnPage('https://www.americanas.com.br');
+    I.click('olá, faça seu login ou cadastre-se')
+    I.click('cadastrar')
+    I.click('Nome completo')
+    I.fillField('Nome completo', 'MARIA LUIZA DA SILVA')
+    I.click('Feminino')
+    I.click('//*[@type="tel"]')
+    I.fillField('//*[@type="tel"]', ('22032000'))
+    I.click('//*[@name="cpf"]')
+    I.fillField('//*[@name="cpf"]', ('11111111111'))
+    I.click('//*[@name="phone"]')
+    I.fillField('//*[@name="phone"]', ('11988998899'))
+    I.click('//*[@name="email"]')
+    I.fillField('//*[@name="email"]', 'testeluiza@gmail.com')
+    I.click('//*[@name="password"]')
+    I.fillField('//*[@name="password"]', ('105567432'))
+    I.click('//*[@type="submit"]')
+})
+Scenario('realizar cadastro com cpf invalido', ({ I }) => {
+
+    I.amOnPage('https://www.americanas.com.br');
+    I.click('olá, faça seu login ou cadastre-se')
+    I.click('cadastrar')
+    I.click('Nome completo')
+    I.fillField('Nome completo', 'MARIA LUIZA DA SILVA')
+    I.click('Feminino')
+    I.click('//*[@type="tel"]')
+    I.fillField('//*[@type="tel"]', ('22032000'))
+    I.click('//*[@name="cpf"]')
+    I.fillField('//*[@name="cpf"]', ('11111111111'))
+    I.click('//*[@name="phone"]')
+    I.fillField('//*[@name="phone"]', ('11988998899'))
+    I.click('//*[@name="email"]')
+    I.fillField('//*[@name="email"]', 'testeluiza@gmail.com')
+    I.click('//*[@name="password"]')
+    I.fillField('//*[@name="password"]', ('105567432'))
+    I.click('//*[@type="submit"]')
+
+})
+
+
+
+
+
+
+
+
