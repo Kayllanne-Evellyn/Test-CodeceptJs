b0d3ac9f48d87d7a4d87dbcd2a2c0de363e03668
Kayllanne-Evellyn <kayllanne373evellyn@gmail.com>
2023-08-05 16:57:35 -0300

    refatoramento do código

--- cadastro_test.js
+++ cadastro_test.js
@@ -20,7 +20,7 @@ Scenario('realizar cadastro', ({ I }) => {
     I.fillField('//*[@name="password"]', ('105567432'))
     I.click('//*[@type="submit"]')
 
-});
+}).tag('sucesso')
 
 Scenario('realizar cadastro com cpf invalido', ({ I }) => {
 
@@ -41,8 +41,10 @@ Scenario('realizar cadastro com cpf invalido', ({ I }) => {
     I.click('//*[@name="password"]')
     I.fillField('//*[@name="password"]', ('105567432'))
     I.click('//*[@type="submit"]')
-})
-Scenario('realizar cadastro com cpf invalido', ({ I }) => {
+
+}).tag('cpf invalido')
+
+Scenario('realizar cadastro sem data de nascimento', ({ I }) => {
 
     I.amOnPage('https://www.americanas.com.br');
     I.click('olá, faça seu login ou cadastre-se')
@@ -50,8 +52,6 @@ Scenario('realizar cadastro com cpf invalido', ({ I }) => {
     I.click('Nome completo')
     I.fillField('Nome completo', 'MARIA LUIZA DA SILVA')
     I.click('Feminino')
-    I.click('//*[@type="tel"]')
-    I.fillField('//*[@type="tel"]', ('22032000'))
     I.click('//*[@name="cpf"]')
     I.fillField('//*[@name="cpf"]', ('11111111111'))
     I.click('//*[@name="phone"]')
@@ -62,7 +62,7 @@ Scenario('realizar cadastro com cpf invalido', ({ I }) => {
     I.fillField('//*[@name="password"]', ('105567432'))
     I.click('//*[@type="submit"]')
 
-})
+}).tag('sem data de nascimento')
 
 
 
